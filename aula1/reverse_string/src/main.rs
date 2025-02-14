ab76ae8f21670e85df45d081751cfa0779047d5a
PiquenoBruno <brunofirmino960@gmail.com>
2025-02-14 00:30:28 +0000

    main

--- aula1/reverse_string/src/main.rs
+++ aula1/reverse_string/src/main.rs
@@ -6,10 +6,6 @@ fn main() {
     let mut input = String::new();
     println!("Digite uma palavra:");
     io::stdin().read_line(&mut input).expect("Falha ao ler a linha");
-
-   
-    let input = input.trim();
-
     
     let reversed = utils::inverter(&input);
     println!("Palavra invertida: {}", reversed);
